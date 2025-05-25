959fa2af10774c89eac4b794e97f87e8026ce6d4
bebbo <bebbo@bejy.net>
2025-05-25 21:34:33 +0200

    rework/fix time functions

--- sources/nix/time/clock_gettime.c
+++ sources/nix/time/clock_gettime.c
@@ -6,8 +6,12 @@
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
 	if (tp) {
-		GetSysTime((struct timeval *)tp);
-		tp->tv_nsec /= 1000;
+		struct DateStamp stamp;
+		DateStamp(&stamp);
+
+		long s = stamp.ds_Tick / TICKS_PER_SECOND;
+		tp->tv_sec = (stamp.ds_Days * 24 * 60 + stamp.ds_Minute) * 60 + _timezone + s + 252460800;
+		tp->tv_nsec = (stamp.ds_Tick * (1000000 / TICKS_PER_SECOND) - s * 1000000) * 1000;
 	}
 	return 0;
 }
